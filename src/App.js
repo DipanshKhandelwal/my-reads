768e4be7c01714a47be39c3c1f438ec32b58ca3a
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-12-23 21:40:37 +0530

    Connect API fetch books

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './API/BooksAPI'
 import './App.css'
 import { Route } from "react-router-dom";
 import Search from './Search/Search'
 import BooksPage from './BooksPage/BooksPage'
 
 class BooksApp extends React.Component {
+
+  state = {
+    books : []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books)=> this.setState({ books }) )
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path='/' render={() => (
-          <BooksPage />
+          <BooksPage 
+            books={this.state.books}
+          />
         )} />
         <Route path='/search' render={() => (
           <Search />
