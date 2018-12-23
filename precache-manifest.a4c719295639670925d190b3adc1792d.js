9fb0e30dd562ae54f165370fc9363a11665cb8fb
DipanshKhandelwal <deepanshrocks438@gmail.com>
2018-12-24 00:49:05 +0530

    Updates

---
+++ precache-manifest.a4c719295639670925d190b3adc1792d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "d3c91a85274073bd8c52",
+    "url": "/my-reads/static/js/runtime~main.d3c91a85.js"
+  },
+  {
+    "revision": "075ffcf8ff488b1916ea",
+    "url": "/my-reads/static/js/main.075ffcf8.chunk.js"
+  },
+  {
+    "revision": "7921b822a240e9d4d5e7",
+    "url": "/my-reads/static/js/1.7921b822.chunk.js"
+  },
+  {
+    "revision": "075ffcf8ff488b1916ea",
+    "url": "/my-reads/static/css/main.4084e752.chunk.css"
+  },
+  {
+    "revision": "40c55fd9d0e64f5a5ae626d1c82739ab",
+    "url": "/my-reads/index.html"
+  }
+];
